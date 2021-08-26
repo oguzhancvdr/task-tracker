4c3dd109fcfa69be43987b4b4efb517824f2df7b
ogzhn <oguzhancavdar06@gmail.com>
2021-08-26 21:46:03 +0300

    create CreateTask.js comp

---
+++ src/components/CreateTask.js
@@ -0,0 +1,39 @@
+import {useState} from 'react'
+
+export default function CreateTask() {
+    const [text, setText] = useState("")
+    const [day, setDay] = useState("")
+
+    const handleTextChange = (e) => setText(e.target.value)
+    const handleDayChange = (e) => setDay(e.target.value)
+console.log(`formValues`, {text, day})
+    return (
+        <form className="add-form">
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            id="task"
+            name="text"
+            type="text"
+            placeholder="Enter a task name"
+            // required
+            value={text}
+            onChange={handleTextChange}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            id="day"
+            name="day"
+            type="text"
+            placeholder="Enter a day and time"
+            // required
+            value={day}
+            onChange={handleDayChange}
+          />
+        </div>
+        <input type="submit" value="Save Task" className="btn btn-block" />
+      </form>
+    )
+}
